19acd0f77f5cd7c1dbb759dc9a5fd8bc09a4e94b
kevinkyyan <kevin.yan@ubc.ca>
2024-10-06 08:25:55 -0700

    Update validation code_ Oct 6 2024

--- R/validation_demographic.R
+++ R/validation_demographic.R
@@ -7,12 +7,31 @@ USSimulation <-read_csv("data-raw/USSimulation.csv")
 # run epic
 init_session()
 settings<- get_default_settings()
-n_sim <- setting$n_base_agents
+n_sim <- settings$n_base_agents
 inputs <- Cget_inputs()
 time_horizon <- inputs$global_parameters$time_horizon
 run()
 output <- Cget_output_ex()
 
-epic_popsize<- data.frame(1:time_horizon,output$n_alive_by_ctime_sex)
-names(epic_popsize) <- c("year", "male", "female")
-epic
+# create dataframe of EPIC population size by age
+epic_popsize_age<- data.frame(1:time_horizon,output$n_alive_by_ctime_age)
+colnames(epic_popsize_age)[1] <- "year"
+colnames(epic_popsize_age)[2:ncol(epic_popsize_age)] <- 1:(ncol(epic_popsize_age) - 1)
+epic_popsize_age <- epic_popsize_age[, -(2:40)]
+epic_popsize_age$year <- seq(1, nrow(epic_popsize_age))
+
+# notes transform horizontal into vertical
+
+# create dataframe of EPIC population size by sex
+epic_popsize_sex<- data.frame(1:time_horizon,output$n_alive_by_ctime_sex)
+names(epic_popsize_sex) <- c("year", "male", "female")
+epic_popsize_sex$overall<- epic_popsize_sex$male + epic_popsize_sex$female
+epic_popsize_sex$year <- seq(1, nrow(epic_popsize_sex))
+
+
+
+
+
+
+
+
