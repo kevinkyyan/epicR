7404880b53d697b5d3991f2157ae8d8d95a5e8b7
kevinkyyan <kevin.yan@ubc.ca>
2024-10-17 11:52:16 -0700

    Adjusted growth rate

--- R/validation_demographic.R
+++ R/validation_demographic.R
@@ -2,7 +2,7 @@ library(epicR)
 library(tidyverse)
 
 # load census validation targets
-USSimulation <-read_csv("data-raw/USSimulation.csv") #check NAs
+USSimulation <-read_csv("data-raw/USSimulation.csv")
 
 # run epic
 settings<- get_default_settings()
@@ -31,6 +31,44 @@ epic_popsize_age$year <- seq(2015, by = 1, length.out = nrow(epic_popsize_age))
 epic_popsize_age_long <- epic_popsize_age %>%
                               pivot_longer(!year, names_to = "age", values_to = "EPIC_popsize") %>%
                                 mutate(age=as.integer(age))
+# merge US sim and EPIC pop outputs
+validate_pop_size <-  USSimulation %>%
+  rename(US_popsize=value) %>%
+  left_join(epic_popsize_age_long, by=c("year","age"))
+
+# scaled population graph
+library(ggplot2)
+library(ggthemes)
+
+# Define the 'year' and 'savePlots' variables as needed
+
+savePlots <- TRUE  # Set this to TRUE if you want to save the plot, FALSE if you don't
+year <- 2035  # Example year, adjust as needed
+
+# Prepare the data for plotting
+dfPredicted <- data.frame(population = USpop_scaled$US_popsize_scaled, age = USpop_scaled$age)
+dfSimulated <- data.frame(population = USpop_scaled$EPIC_popsize_scaled, age = USpop_scaled$age)
+
+# Make the simulated population negative for the pyramid effect
+dfSimulated$population <- dfSimulated$population * (-1)
+
+# Create the plot
+p <- ggplot(NULL, aes(x = age, y = population)) +
+  theme_tufte(base_size = 14, ticks = FALSE) +  # Minimal theme
+  geom_bar(aes(fill = "Simulated"), data = dfSimulated, stat = "identity", alpha = 0.5) +  # Simulated population in blue
+  geom_bar(aes(fill = "Predicted"), data = dfPredicted, stat = "identity", alpha = 0.5) +  # Predicted population in red
+  theme(axis.title = element_blank()) +  # Remove axis titles
+  ggtitle("Simulated vs. Predicted Population Pyramid") +  # Add plot title
+  theme(legend.title = element_blank()) +  # Remove legend title
+  scale_y_continuous(name = "Population", labels = scales::comma) +  # Format y-axis with commas
+  scale_x_continuous(name = "Age", labels = scales::comma)  # Format x-axis for age
+# Save the plot if 'savePlots' is TRUE
+if (savePlots) {
+  ggsave(paste0("Population_", year, ".tiff"), plot = p, device = "tiff", dpi = 300)
+}
+
+# Display the plot
+plot(p)
 
 
 # create dataframe of EPIC population size by sex
@@ -39,10 +77,6 @@ names(epic_popsize_sex) <- c("year", "male", "female")
 epic_popsize_sex$overall<- epic_popsize_sex$male + epic_popsize_sex$female
 epic_popsize_sex$year <- seq(1, nrow(epic_popsize_sex))
 
-# merge US sim and EPIC pop outputs
-validate_pop_size <-  USSimulation %>%
-                            rename(US_popsize=value) %>%
-                            left_join(epic_popsize_age_long, by=c("year","age"))
 
 
 
