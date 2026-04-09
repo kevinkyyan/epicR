858ff7c28aa694780d0ff8558c9f4bb419b83f92
Kevin Yan <kevinyan@host133-97.vpn.ubc.ca>
2026-04-08 20:32:22 -0700

    Revised ADI integration to test

--- src/model_input.cpp
+++ src/model_input.cpp
@@ -194,7 +194,6 @@ int set_input_var(std::string name, NumericVector value)
 
   if(name=="COPD$ln_h_COPD_betas_by_sex") READ_R_MATRIX(value,input.COPD.ln_h_COPD_betas_by_sex);
   if(name=="COPD$logit_p_COPD_betas_by_sex") READ_R_MATRIX(value,input.COPD.logit_p_COPD_betas_by_sex);
-  if(name=="COPD$adi_logit_p_COPD_offset") READ_R_VECTOR(value,input.COPD.adi_logit_p_COPD_offset);
   if(name=="COPD$adi_h_COPD_factors") READ_R_VECTOR(value,input.COPD.adi_h_COPD_factors);
 
   if(name=="lung_function$pred_fev1_betas_by_sex") READ_R_MATRIX(value,input.lung_function.pred_fev1_betas_by_sex);
